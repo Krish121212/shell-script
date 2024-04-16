6f8067910dab5a2f3ad091a1c5c769e42bd60ab3
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-17 00:37:33 +0530

    Scripts

---
+++ variables1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash/
+
+person1=$1
+person2=$2
+person3=$3
+
+echo "$person1: Hi $person2 how are you"
+echo "$person2: Hi $person1 i'm good, where is $person3"
+echo "$person1: I don't know about $person3"
+echo "$person3: i'm here"
