0648cecf043ac69ba110a1ec1eb49ec1f3f14327
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-17 01:25:35 +0530

    Scripts

--- conditions.sh
+++ conditions.sh
@@ -3,8 +3,10 @@
 number=$1
 
 if ($number>5){
-    echo "entered number is more than 5"
+    then
+    echo "entered $number is more than 5"
 
     else 
-    echo "entered number is less than 5"
+    echo "entered $number is less than 5"
+    fi
 }
