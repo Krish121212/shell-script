0d886c78c9fce875ed29f031782642b523982a41
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-17 01:03:09 +0530

    Scripts

--- arrays.sh
+++ arrays.sh
@@ -2,6 +2,6 @@
 
 sisters=("bhargavi" "jaya" "kv")
 
-echo "Own sister: ${sisters[0]}
-echo "HCL sister: ${HCL[1]}
-echo "HCL sisters: ${HCL[2]}
+echo "Own sister: ${sisters[0]}"
+echo "HCL sister: ${HCL[1]}"
+echo "HCL sisters: ${HCL[2]}"
