4e42a94949a4a2a5b3a2702b2047de9371546fc0
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-17 14:27:59 +0530

    Scripts

--- install_package.sh
+++ install_package.sh
@@ -8,6 +8,6 @@ then
     echo "please run $package package with super user access"
     exit 1
 else
-    dnf install $package -y | cat /c/users/surya/Desktop/Devops/repos/shell-script/status
-    echo "installation of $package package is Success" | mutt -a "/c/users/surya/Desktop/Devops/repos/shell-script/status" -s "install status" maladikrishnakishore@gmail.com
+    dnf install $package -y | cat > /c/users/surya/Desktop/Devops/repos/shell-script/status
+    echo "installation of $package package is Success" | mail -a "/c/users/surya/Desktop/Devops/repos/shell-script/status" -s "install status" maladikrishnakishore@gmail.com
 fi
