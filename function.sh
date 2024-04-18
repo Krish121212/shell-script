951094c7799f2c669970bb6ecabb573b9ee6010f
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-19 00:18:58 +0530

    Scripts

--- function.sh
+++ function.sh
@@ -1,5 +1,22 @@
 #!/bin/bash/
 
-validate(){
-    $0
+userid=$(id -u)
+
+VALIDATE(){
+    echo "Exit status: $1"
+    echo "Installing: $2"
 }
+
+if [ $userid != 0 ]
+then
+    echo "please run mysql package with super user access"
+    exit 1
+else
+    echo "you are super user"
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
+
+dnf install nginx -y
+VALIDATE $? "Installing nginx"
