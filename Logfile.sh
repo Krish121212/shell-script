46ee6fcc6cb88af6b6c4aacc7ed7071357f889f8
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-20 00:33:59 +0530

    Scripts

---
+++ Logfile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/
+
+userid=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp
+install=$1
+install1=$2
+
+Validate(){
+if [ $userid != 0 ]
+then
+    echo "please run package with super user access: failure"
+    exit 1
+else
+    echo "you are super user: success"
+fi
+}
+
+dnf install $install -y
+Validate $? "Installing $install"
+
+dnf install $install1 -y
+Validate $? "Installing $install1"
