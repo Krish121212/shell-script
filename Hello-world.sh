740fd8bd8b073d9e3a347c1ad3ce33c670506def
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-20 23:43:01 +0530

    Colors

--- Hello-world.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash/
-
-echo "Hi, This is KK. I'm learning DevOps"
--- colors.sh
+++ colors.sh
@@ -6,19 +6,21 @@ SCRIPTNAME=$(echo $0 | cut -d '.' -f1)
 LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 install=$1
 install1=$2
-
+Red="\e[31m"
+Green="\e[32m"
+Nor="\e[0m"
 Validate(){
 if [ $userid != 0 ]
 then
-    echo "please run package with super user access: failure"
+    echo -e "please run package with super user access: $Red FAILURE $Nor"
     exit 1
 else
-    echo "you are super user: success"
+    echo -e "you are super user: $Green SUCCESS $Nor"
 fi
 }
 
-dnf install $install -y &>>$LOGFILE
-Validate $? "Installing $install"
+dnf install $install -y 
+Validate $? echo -e "Installing $install: $Green SUCCESS $Nor"
 
-dnf install $install1 -y &>>$LOGFILE
-Validate $? "Installing $install1"
+dnf install $install1 -y 
+Validate $? echo -e "Installing $install1: $Green SUCCESS $Nor"
