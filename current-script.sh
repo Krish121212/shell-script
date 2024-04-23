b8e19d1e0bb355a49c40eb373234f783d0bb7834
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-24 01:17:28 +0530

    calling other script

--- current-script.sh
+++ current-script.sh
@@ -12,5 +12,6 @@ echo "PID: $$"
 ## PID will be different for above approach
 
 source /home/ec2-user/shell-script/Other-script.sh
+## PID will be sae in this approach 
 
 echo -e "This is $NAME2 from $G current $N SCRIPT"
---
+++ delete-logs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash/
+
+source_directory=/tmp/
+
+Red="\e[31m"
+Green="\e[32m"
+Nor="\e[0m"
+
+if [ -d $source_directory ]
+then
+    echo -e "$Green source directory exists $Nor"
+else
+    echo -e "$Red source dirctory doesn't exists $Nor"
+    exit 1
+fi
