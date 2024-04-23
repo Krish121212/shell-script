81cb025739d0e107a64bc0f6fad15e6648db0727
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-23 16:42:54 +0530

    DB script

--- Other-script.sh
+++ Other-script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash/
 
-NAME1=Lalitha
+NAME3=Lalitha
 Y="\e[33m"
 N="\e[0m"
 
 echo "PID: $$"
 
 
-echo -e "This is $NAME1 from $Y other $N SCRIPT"
+echo -e "This is $NAME3 from $Y other $N SCRIPT"
