1e32da82eee5deaffd331b357edc76f3372b3288
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-23 13:49:58 +0530

    DB script

---
+++ Other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+
+NAME1=Lalitha
+Y="\e[33m"
+N="\e[0m"
+echo -e "This is $NAME1 from $Y other $N SCRIPT"
---
+++ current-script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/
+
+NAME1=KRISHNA
+NAME2=Vaishnavi
+G="\e[32m"
+N="\e[0m"
+
+echo -e "This is $NAME1 from $G current $N SCRIPT"
+
+./Other.script.sh
+
+echo -e "This is $NAME2 from $G current $N SCRIPT"
