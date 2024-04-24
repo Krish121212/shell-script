c108b919401b3e51038cfafb6ae8f48fae506e78
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-25 00:42:30 +0530

    calling other script

--- archiving.sh
+++ archiving.sh
@@ -1,9 +1,10 @@
 #!/bin/bash/
 
-SOURCE_DIR=/home/ec2-user/shell-script/source_dir/
+SOURCE_DIR=/home/ec2-user/shell-script/source_dir
 DESTINATION_DIR=/home/ec2-user/shell-script/destination_dir/
 TIMESTAMP=$(date +%F-%H-%M-%S)
+FILENAME=archive-$TIMESTAMP.tar.gz
 
 
-tar -czvf archive-$TIMESTAMP.tar.gz $SOURCE_DIR
-mv -t $SOURCE_DIRarchive-$TIMESTAMP.tar.gz  $DESTINATION_DIR
+tar -czvf $FILENAME $SOURCE_DIR
+mv $SOURCE_DIR/$FILENAME  $DESTINATION_DIR
