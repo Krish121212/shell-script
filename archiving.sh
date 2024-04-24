1fa0795c36988ca3d622870994fd1775b7d3ac03
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-25 00:57:06 +0530

    calling other script

--- archiving.sh
+++ archiving.sh
@@ -5,8 +5,10 @@ DESTINATION_DIR=/home/ec2-user/shell-script/destination_dir/
 TIMESTAMP=$(date +%F-%H-%M-%S)
 FILENAME=archive-$TIMESTAMP.tar.gz
 OUTPUT=/tmp/archive-$TIMESTAMP.tar.gz
+Green="\e[32m"
+Nor="\e[0m"
 
 
 tar -czvf $FILENAME $SOURCE_DIR &>>$OUTPUT
 mv $FILENAME  $DESTINATION_DIR
-echo "Files in source dir have been archived and moved to destination dir"
+echo -e "$Green Files in source dir have been archived and moved to destination dir $Nor"
