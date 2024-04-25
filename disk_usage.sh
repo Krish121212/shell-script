ed150094ae465e385a3e7ee58c9fad432ac514f6
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-26 00:23:30 +0530

    calling other script

---
+++ disk_usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash/
+
+DISKUSAGE=$(df -hT | grep xfs)
+THRESOULD=5
+
+while IFS= read -r line
+do 
+    USAGE= $(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
+    FOLDER= $(echo $line | awk -F " " '{print $Nf}')
+    if [ $USAGE >= $THRESOULD ]
+    then 
+        echo "For $FOLDER usage is ore than $THRESOULD: current usage: $USAGE" 
+    fi
+done  <<< $DISKUSAGE
+
+
+
+
+
