d3f30a8b3ef341db013b3189db496b9f96199f0c
surya pelluru <maladikrishnakishore@gmail.com>
2024-04-26 00:29:53 +0530

    calling other script

--- disk_usage.sh
+++ disk_usage.sh
@@ -6,7 +6,7 @@ THRESOULD=5
 while IFS= read -r line
 do 
     USAGE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
-    FOLDER=$(echo $line | awk -F " " '{print $Nf}')
+    FOLDER=$(echo $line | awk -F " " '{print $7f}')
     if [ $USAGE -ge $THRESOULD ]
     then 
         echo "For $FOLDER usage is more than $THRESOULD: current usage: $USAGE" 
